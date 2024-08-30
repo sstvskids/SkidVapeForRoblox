e7955140e7c6b6890fbcf0e6f5b752db46c6742c
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-08-31 09:59:58 +1000

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2017,7 +2017,7 @@ local function loadVape()
 end
 
 task.spawn(function()
-	local cheatengineexecutors = {"solara", "celery", "feather", "mantiWPF", "octane", "appleware"}
+	local cheatengineexecutors = loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/SkidVapeForRoblox/main/Libraries/executordetector.lua", true))()
 	local executorid = identifyexecutor()
 	if identifyexecutor then
 	    local executor = string.lower(executorid)

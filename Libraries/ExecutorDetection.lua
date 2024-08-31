dd771c707faba858a87d93df2b9701c536dec312
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-09-01 08:18:06 +1000

    Update ExecutorDetection.lua

--- Libraries/ExecutorDetection.lua
+++ Libraries/ExecutorDetection.lua
@@ -1,1 +1,1 @@
-return {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex", "Jules" "Cubix iOS" --[[idk whats it called ok man--]]}
+return {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex", "Jules", "Cubix iOS"}

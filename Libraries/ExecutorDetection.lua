11bee0e84e63a6e4e237da15de3d2b8c13a89fd0
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-08-31 16:18:38 +1000

    Update ExecutorDetection.lua

--- Libraries/ExecutorDetection.lua
+++ Libraries/ExecutorDetection.lua
@@ -1,1 +1,1 @@
-return {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex"}
+return {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex", "Jules"}

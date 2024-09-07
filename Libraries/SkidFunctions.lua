6cc71fd1e2979a66be861088b31041a25478270d
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-09-07 10:50:43 +1000

    Create SkidFunctions.lua

---
+++ Libraries/SkidFunctions.lua
@@ -0,0 +1,25 @@
+local skidstore = {
+	cheatengine = loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/SkidVapeForRoblox/main/Libraries/ExecutorDetection.lua", true))(),
+	skidver = "Next-Gen",
+	skiduser = game.Players.LocalPlayer.Username,
+	whitelist = shared.vapewhitelist
+}
+
+skidstore.CheatEngineCheck = function({
+	if identifyexecutor then
+	    local executor = string.lower(identifyexecutor())
+	    for i, v in pairs(skidstore.cheatengine) do
+	        if string.find(executor, string.lower(v)) then
+				local frame = GuiLibrary.CreateNotification("Vape", "Executor is not supported. Check console for more information, regarding unsupported executors. ("..executorid..") ", 60, "assets/WarningNotification.png")
+				frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 255, 255)			
+			end
+	    end
+	end
+})
+
+skidstore.VersionCheck = function({
+	warningNotification("Skid-Vape "..skidstore.skidver.."", "Logged in as "..skidstore.skiduser.." Whitelist: "..skidstore.whitelist.."", 6.25)
+})
+
+getgenv().SkidStore = SkidStore;
+return SkidStore

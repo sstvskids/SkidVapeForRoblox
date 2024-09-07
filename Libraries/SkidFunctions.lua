f20f280fc36403a619c9ebebb8767844e351daff
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-09-07 11:06:32 +1000

    Update SkidFunctions.lua

--- Libraries/SkidFunctions.lua
+++ Libraries/SkidFunctions.lua
@@ -5,21 +5,21 @@ local skidstore = {
 	whitelist = shared.vapewhitelist
 }
 
-skidstore.CheatEngineCheck = function({
+skidstore.CheatEngineCheck = function()
 	if identifyexecutor then
 	    local executor = string.lower(identifyexecutor())
 	    for i, v in pairs(skidstore.cheatengine) do
 	        if string.find(executor, string.lower(v)) then
-				local frame = GuiLibrary.CreateNotification("Vape", "Executor is not supported. Check console for more information, regarding unsupported executors. ("..executorid..") ", 60, "assets/WarningNotification.png")
+				local frame = GuiLibrary.CreateNotification("Vape", "Executor is not supported. Check console for more information, regarding unsupported executors. ("..identifyexecutor()..") ", 60, "assets/WarningNotification.png")
 				frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 255, 255)			
 			end
 	    end
 	end
-})
+end
 
-skidstore.VersionCheck = function({
+skidstore.VersionCheck = function()
 	warningNotification("Skid-Vape "..skidstore.skidver.."", "Logged in as "..skidstore.skiduser.." Whitelist: "..skidstore.whitelist.."", 6.25)
-})
+end
 
 getgenv().SkidStore = SkidStore;
 return SkidStore

69c4f0f196ee3d66581c021743db19a2182ca9e9
nexus <nebulawareconfigs@gmail.com>
2024-09-07 21:29:09 -0700

    Update SkidFunctions.lua

--- Libraries/SkidFunctions.lua
+++ Libraries/SkidFunctions.lua
@@ -1,5 +1,5 @@
 local skidstore = {
-	cheatengine = {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex", "Jules", "Cubix iOS" "Delta iOS"},
+	cheatengine = {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex", "Jules", "Cubix iOS", "Delta iOS"},
 	skidver = "Next-Gen",
 	skiduser = game.Players.LocalPlayer.Name
 }

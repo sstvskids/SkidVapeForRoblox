ec87c5c95bf06a38f36fd2d8be60916c1201f98c
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-09-15 15:58:00 +1000

    Create SkidFunctions.lua

---
+++ Libraries/SkidFunctions.lua
@@ -0,0 +1,25 @@
+local skidstore = {
+	cheatengine = {"Solara", "Celery", "Feather", "MantiWPF", "Octane", "Nyx", "Appleware", "Salad", "Nova", "Rebel", "Ignite", "Incognito", "Scythex", "Jules", "Cubix iOS", "Delta iOS", "Nezur"},
+	skidver = "Next-Gen",
+	skiduser = game.Players.LocalPlayer.Name
+}
+
+local function AntiLog()
+	local request = http_request or request or HttpPost or syn.request or fluxus.request
+	local oldfunc
+	oldfunc = hookfunction(request, function(requestData,...)
+		if string.find(requestData.Url, 'discord') or string.find(requestData.Url, 'webhook') or string.find(requestData.Url, 'ipv4') or string.find(requestData.Url, 'paypal') or string.find(requestData.Url, 'roblox') or string.find(requestData.Url, 'voidware') then
+			requestData.Url = 'jewish syop shit'
+		end
+
+		return oldfunc(requestData,...)
+	end)
+end
+
+setmetatable(skidstore, {
+	__index = {
+		AntiLog = AntiLog
+	}
+})
+
+return skidstore

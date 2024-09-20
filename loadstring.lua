96b019c08a9bbee3d1f0b3904798e805e8c25dcb
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-09-20 19:40:58 +1000

    Update and rename loadstring to loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,5 @@
+if loadstring then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/SkidVapeForRoblox/main/NewMainScript.lua", true))()
+else
+  warn("executor not supported!!21@!, but seriously, please get solara or wave!!")
+end

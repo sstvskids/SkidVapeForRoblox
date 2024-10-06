5619c50dcf856391d9dde214bff3868a14937470
sstvskids <staavstudios@gmail.com>
2024-10-07 10:22:59 +1100

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -8995,7 +8995,7 @@ run(function()
 							local item = getItemNear("scythe")
 							networkticks = networkticks + 1
 							if ScytheToggle.Enabled and item and lplr.Character.HandInvItem.Value == item.tool and bedwars.CombatController then
-								if NetworkHelper.Enabled and networkbypass == true and networkticks >= ScytheSpeed.Value + SpeedValue.Value then
+								if NetworkHelper.Enabled and networkbypass == true and networkticks >= ScytheSpeed.Value then
 									pcall(function()
 										sethiddenproperty(entityLibrary.character.HumanoidRootPart, "NetworkIsSleeping", false)
 										networkticks = 0

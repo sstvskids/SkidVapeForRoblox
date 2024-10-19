9b8e998e9e30ad3296bb91b519529adbb23c5922
sstvskids <staavstudios@gmail.com>
2024-10-19 13:38:51 +1100

    optimized in 1 line 🤑

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -151,11 +151,7 @@ local function warningNotification(title, text, delay)
 	return (suc and res)
 end
 
-local function run(func)
-	task.spawn(function()
-		local suc, err = pcall(function() func() end) if not suc then warningNotification('Vape', 'Failed to load module: '..tostring(err), 999) end
-	end)
-end
+local function run(func) task.spawn(function() local suc, err = pcall(function() func() end) if not suc then warningNotification('Vape', 'Failed to load module: '..tostring(err), 999) end end) end
 
 if not suc then return warningNotification('Vape', 'Failed to load module: '..tostring(err), 8) end
 

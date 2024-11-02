4b92c3357b46279d1dc42f661a368d28ac5d54a1
sstvskids <staavstudios@gmail.com>
2024-11-03 08:13:41 +1100

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -2557,7 +2557,6 @@ run(function()
 						if bedwars.matchState == 0 then return end
 					end
 					if entityLibrary.isAlive then
-						local item = getItemNear("scythe")
 						local playerMass = (entityLibrary.character.HumanoidRootPart:GetMass() - 1.4) * (delta * 100)
 						flyAllowed = ((lplr.Character:GetAttribute("InflatedBalloons") and lplr.Character:GetAttribute("InflatedBalloons") > 0) or store.matchState == 2 or megacheck) and 1 or 0
 						playerMass = playerMass + (flyAllowed > 0 and 4 or 0) * (tick() % 0.4 < 0.2 and -1 or 1)
@@ -2573,11 +2572,7 @@ run(function()
 							onground = newray and true or false
 							if lastonground ~= onground then
 								if (not onground) then
-									if Disabler.Enabled and item and lplr.Character.HandInvItem.Value == item.tool then
-										groundtime = tick() + (120.1 + (entityLibrary.groundTick - tick()))
-									else
-										groundtime = tick() + (2.6 + (entityLibrary.groundTick - tick()))
-									end
+									groundtime = tick() + (2.6 + (entityLibrary.groundTick - tick()))
 									if FlyAnywayProgressBarFrame then
 										FlyAnywayProgressBarFrame.Frame:TweenSize(UDim2.new(0, 0, 0, 20), Enum.EasingDirection.InOut, Enum.EasingStyle.Linear, groundtime - tick(), true)
 									end
@@ -2588,11 +2583,7 @@ run(function()
 								end
 							end
 							if FlyAnywayProgressBarFrame then
-								if item and lplr.Character.HandInvItem.Value == item.tool then
-									FlyAnywayProgressBarFrame.TextLabel.Text = math.max(onground and 120 or math.floor((groundtime - tick()) * 10) / 10, 0).."s"
-								else
-									FlyAnywayProgressBarFrame.TextLabel.Text = math.max(onground and 2.6 or math.floor((groundtime - tick()) * 10) / 10, 0).."s"
-								end
+								FlyAnywayProgressBarFrame.TextLabel.Text = math.max(onground and 2.6 or math.floor((groundtime - tick()) * 10) / 10, 0).."s"
 							end
 							lastonground = onground
 						else

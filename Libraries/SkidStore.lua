a0b7534da4947a98ace8873482579d3bfd7a7fad
sstvskids <staavstudios@gmail.com>
2024-11-03 17:41:46 +1100

    clean

--- Libraries/SkidStore.lua
+++ Libraries/SkidStore.lua
@@ -1,27 +1,18 @@
-local lplr = cloneref(game:GetService("Players")).LocalPlayer
-local skidstore = {
-	skidver = "Next-Gen",
-	skiduser = lplr.Name,
-	skiduserid = lplr.UserId,
-	skids = {"erco", "coco", "daiplayz"}
+local lplr = cloneref(game:GetService("Players")).LocalPlayer;
+local skidstore : table = {
+    skidver = 'Next-Gen';
+    skiduser = lplr.Name;
+    skiduserid = lplr.UserId;
+    AntiLog = function()
+        local request = http_request or request or HttpPost or syn.request or fluxus.request;
+        local oldfunc;
+        oldfunc = hookfunction(request, function(requestData,...)
+            if string.find(requestData.Url, 'discord' or 'webhook' or 'ipv4' or 'paypal' or 'roblox' or 'voidware') then
+                requestData.Url = nil;
+            end;
+        end);
+        return oldfunc;
+    end;
 }
-
-local function AntiLog()
-	local request = http_request or request or HttpPost or syn.request or fluxus.request
-	local oldfunc
-	oldfunc = hookfunction(request, function(requestData,...)
-		if string.find(requestData.Url, 'discord') or string.find(requestData.Url, 'webhook') or string.find(requestData.Url, 'ipv4') or string.find(requestData.Url, 'paypal') or string.find(requestData.Url, 'roblox') or string.find(requestData.Url, 'voidware') then
-			requestData.Url = 'jewish syop shit'
-		end
-
-		return oldfunc(requestData,...)
-	end)
-end
-
-setmetatable(skidstore, {
-	__index = {
-		AntiLog = AntiLog
-	}
-})
 
 return skidstore

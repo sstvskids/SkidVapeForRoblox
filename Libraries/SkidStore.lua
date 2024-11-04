83c4029f5221deb3e35f7329b65af0dcba056319
AbyssForRoblox <9dv8jqda@mailmask.me>
2024-11-03 20:03:56 -0800

    Update SkidStore.lua

--- Libraries/SkidStore.lua
+++ Libraries/SkidStore.lua
@@ -1,5 +1,5 @@
-local lplr = cloneref(game:GetService("Players")).LocalPlayer;
-local skidstore : table = {
+local lplr = game:GetService("Players").LocalPlayer; -- cloneref doesnt work i tried it with httpservice
+local skidstore: table = {
     skidver = 'Next-Gen';
     skiduser = lplr.Name;
     skiduserid = lplr.UserId;
@@ -7,7 +7,7 @@ local skidstore : table = {
         local request = http_request or request or HttpPost or syn.request or fluxus.request;
         local oldfunc;
         oldfunc = hookfunction(request, function(requestData,...)
-            if string.find(requestData.Url, 'discord' or 'webhook' or 'ipv4' or 'paypal' or 'roblox' or 'voidware') then
+            if string.find(requestData.Url, 'discord' or 'webhook' or 'ipv4' or 'paypal' or 'roblox' or 'voidware' or 'grabify' or 'iplogger') then
                 requestData.Url = nil;
             end;
         end);
@@ -15,4 +15,4 @@ local skidstore : table = {
     end;
 }
 
-return skidstore
+return skidstore;

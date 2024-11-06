605b8df201050c25a9990c0a85252cf6f9ce526f
sstvskids <staavstudios@gmail.com>
2024-11-06 19:11:00 +1100

    Update SkidStore.lua

--- Libraries/SkidStore.lua
+++ Libraries/SkidStore.lua
@@ -1,14 +1,17 @@
-local lplr = cloneref(game:GetService("Players")).LocalPlayer or game:GetService("Players").LocalPlayer;
+local lplr = cloneref(game:GetService("Players")).LocalPlayer;
 local skidstore : table = {
     skidver = 'Next-Gen';
     skiduser = lplr.Name;
     skiduserid = lplr.UserId;
     AntiLog = function()
         local request = http_request or request or HttpPost or syn.request or fluxus.request;
+        local blockedrequests : table = {'discord', 'webhook', 'ipv4', 'ipv6', 'paypal', 'roblox', 'voidware'};
         local oldfunc;
         oldfunc = hookfunction(request, function(requestData,...)
-            if string.find(requestData.Url, 'discord' or 'webhook' or 'ipv4' or 'paypal' or 'roblox' or 'voidware') then
-                requestData.Url = nil;
+            for i,v in pairs(blockedrequests) do
+                if string.find(requestData.Url, v) then
+                    requestData.Url = nil;
+                end;
             end;
         end);
         return oldfunc;

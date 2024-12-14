85b60c05c9181d6091347031ff5b2d9665c2509f
sstvskids <staavstudios@gmail.com>
2024-12-15 07:33:12 +1100

    gee, fix for skidstore

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -20,6 +20,16 @@ local bedwars = {}
 local getfunctions
 local origC0 = nil
 local collectionservice = game:GetService("CollectionService")
+local function vapeGithubRequest(scripturl)
+	if not isfile("vape/"..scripturl) then
+		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/sstvskids/SkidVapeForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+		assert(suc, res)
+		assert(res ~= "404: Not Found", res)
+		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
+		writefile("vape/"..scripturl, res)
+	end
+	return readfile("vape/"..scripturl)
+end
 local skidstore = shared.skidstore
 local function GetURL(scripturl)
 	if shared.VapeDeveloper then

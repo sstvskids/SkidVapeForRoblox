fc21e7a92903e8bd6f654392bee4296d3250f71b
sstvskids <staavstudios@gmail.com>
2024-12-15 08:02:56 +1100

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -149,7 +149,7 @@ local function warningNotification(title, text, delay)
 	return (suc and res)
 end
 
-local function run(func) local suc, err = pcall(function() func() end) if not suc then warningNotification('Vape', 'Failed to load module: '..tostring(debug.traceback(err)), 999) end end
+local function run(func) local suc, err = pcall(function() func() end) if not suc then print(tostring(debug.traceback(err))), 999) end end
 
 local function isFriend(plr, recolor)
 	if GuiLibrary.ObjectsThatCanBeSaved["Use FriendsToggle"].Api.Enabled then

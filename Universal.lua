cd9e71554a68de24b717af1e6b8877af529c31b0
sstvskids <staavstudios@gmail.com>
2024-12-18 20:37:29 +1100

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -103,7 +103,14 @@ local function removeTags(str)
 	return (str:gsub("<[^<>]->", ""))
 end
 
-local function run(func) task.spawn(function() local suc, err = pcall(function() func() end) if not suc then warningNotification('Vape', 'Failed to load module: '..tostring(debug.traceback(err)), 999) end end) end
+local function run(func)
+	local suc, err = pcall(function()
+		func()
+	end)
+	if not suc then
+		warningNotification('Vape', 'Error: '..err, 10)
+	end
+end
 
 local function isFriend(plr, recolor)
 	if GuiLibrary.ObjectsThatCanBeSaved["Use FriendsToggle"].Api.Enabled then
